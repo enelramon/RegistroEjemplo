230c7833dc6febc7649098036f39a489ff881f81
Enel Almonte <enelramon@hotmail.com>
2018-09-30 12:51:27 -0400

    Quitando detalle de la primera Clase

--- RegistroEjemplo/Entidades/Personas.cs
+++ RegistroEjemplo/Entidades/Personas.cs
@@ -15,13 +15,6 @@ namespace RegistroEF.Entidades
         public string Cedula { get; set; }
         public string Direccion { get; set; }
         public DateTime FechaNacimiento { get; set; }
-
-        //Lista tipo TelefonoDetalle 
-        // Al colocarle virtual estamos llamando al LazyLoading 
-        /* LazyLoading  consiste en retrasar la carga o inicialización de un objeto hasta el mismo momento de su utilización. 
-         * ... El opuesto de la carga diferida es la carga previa, precarga o eager loading.*/
-
-        public virtual List<TelefonosDetalle> Telefonos { get; set; }
         
         public Personas()
         {
@@ -31,9 +24,6 @@ namespace RegistroEF.Entidades
             Cedula = string.Empty;
             Direccion = string.Empty;
             FechaNacimiento = DateTime.Now;
-
-            Telefonos = new List<TelefonosDetalle>();
-
         }
 
         public Personas(int personaid, string nombre, string telefono, string cedula, string direccion, DateTime fechanacimiento)
@@ -44,7 +34,6 @@ namespace RegistroEF.Entidades
             Cedula = cedula;
             Direccion = direccion;
             FechaNacimiento = fechanacimiento;
-            Telefonos = new List<TelefonosDetalle>();
         }
     }
 }
